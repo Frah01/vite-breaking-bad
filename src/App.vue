089cc7ca0d9df6ea771753d5533329aa33bd0da3
Frah01 <vinario.francesco@gmail.com>
2023-01-13 20:48:54 +0100

    App e list fatti

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <script>
+import axios from 'axios'
+import AppHeader from './components/AppHeader.vue';
+import CharacterList from './components/List.vue';
+import { store } from './components/store.js';
+// loaded
 export default {
-  
+  components: {
+    AppHeader,
+    CharacterList
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  created() {
+    this.getCharacters();
+  },
+  methods: {
+    getCharacters() {
+      axios.get(store.url).then((response) => {
+        store.charactersList = response.data.data
+        setTimeout(() => {
+          store.loading = false
+        }, 2000)
+      })
+    }
+  }
 }
 </script>
 <template lang="">
   <div>
-    
+    <AppHeader message="Yu-GiOh App"/>
+    <main>
+      <CharacterList />
+    </main>
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+@use './styles/general.scss' as *;
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,36 @@
+<script>
+import CharacterCard from './Card.vue';
+import { store } from '../store.js';
+
+export default {
+    components: {
+        CharacterCard,
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>ciccio</div>
+    <div class="container">
+        <div v-if="store.loading">
+            sto caricando
+        </div>
+        <div v-else>
+            <div class="container_card d-flex justify-content-between flex-wrap">
+                <CharacterVard v-for="(item, index) in store.charactersList" :key="index" :character="item"/>
+            </div>
+        </div>
+
+    </div>
+</template>
+<style lang="scss" scoped>
+.container_card {
+    margin: 0 auto;
+    width: 90%;
+    background-color: white;
+}
+</style>
