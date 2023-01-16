be1e844b941e8f7bbf437fefad0bbfe5600520a7
Frah01 <vinario.francesco@gmail.com>
2023-01-16 14:43:16 +0100

    Parte 2

--- src/components/store.js
+++ src/components/store.js
@@ -1,6 +1,6 @@
 import { reactive } from 'vue';
 export const store = reactive({
-    url: "https://db.ygoprodeck.com/api/v7/cardinfo.php?race=insect,Spellcaster",
+    url: "https://db.ygoprodeck.com/api/v7/cardinfo.php?race=insect",
     charactersList: [],
     apiNameParameter: 'name',
 })
